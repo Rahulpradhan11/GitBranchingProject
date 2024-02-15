3de23f4ccc3ef1052c2d707105996844ebba09da
Rahulpradhan11 <prahuljsp88@gmail.com>
2024-02-15 12:00:03 +0530

    Version1 sucessfull

--- Employee.java
+++ Employee.java
@@ -1,9 +1,7 @@
-class Student 
+class Employee
 
 {
-
 	System.out.println("Hell Student");
 	System.out.println(" Welcome to the Git Class");
-
 }
  
---
+++ Location.java
@@ -0,0 +1,9 @@
+class Location 
+
+{
+
+	System.out.println("Hell Student");
+	Scanner sc= new Scanner;
+	System.out.println(" System.in");
+}
+ 
